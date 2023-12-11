9f762cbd45f1cb26425116784c02ec50425261e2
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-11 19:15:00 +0100

    Added EmptyTile class that inherits from the Tile class

---
+++ src/EmptyTile.java
@@ -0,0 +1,15 @@
+public class EmptyTile extends Tile{
+    public EmptyTile(int coordinate) {
+        super(coordinate);
+    }
+
+    @Override
+    public boolean isOccupied() {
+        return false;
+    }
+
+    @Override
+    public Piece getPiece() {
+        return null;
+    }
+}
