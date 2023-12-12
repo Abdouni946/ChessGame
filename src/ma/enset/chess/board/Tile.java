247f869909a8a7468af9ed9b1f51af7afc173297
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-12 21:56:14 +0100

    Added a Map to create all of the Tiles that are valid upfront, and a method to initialise the Map

--- src/ma/enset/chess/board/Tile.java
+++ src/ma/enset/chess/board/Tile.java
@@ -2,8 +2,16 @@ package ma.enset.chess.board;
 
 import ma.enset.chess.pieces.Piece;
 
+import java.util.Map;
+
 public abstract class Tile {
     protected final int coordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILE = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        return null;
+    }
 
     public Tile(final int coordinate) {
         this.coordinate = coordinate;
