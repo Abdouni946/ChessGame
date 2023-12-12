a3b8cb351aeeaa3c44989150b3094e44cbdfe73c
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-12 22:55:55 +0100

    Implemented the Piece class

--- src/ma/enset/chess/pieces/Piece.java
+++ src/ma/enset/chess/pieces/Piece.java
@@ -1,4 +1,18 @@
 package ma.enset.chess.pieces;
 
-public class Piece {
+import ma.enset.chess.Alliance;
+import ma.enset.chess.board.Move;
+
+import java.util.List;
+
+public abstract class Piece {
+    protected final int position;
+    protected final Alliance alliance;
+
+    Piece(final int position, final Alliance alliance) {
+        this.position = position;
+        this.alliance = alliance;
+    }
+
+    public abstract List<Move> calcMoves(final Board board);
 }
