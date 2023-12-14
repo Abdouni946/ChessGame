fad361062153c84cb94caa0eba92ffa81be8bfc6
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-14 20:16:08 +0100

    Added a private constructor to BoardUtils

--- src/ma/enset/chess/board/BoardUtils.java
+++ src/ma/enset/chess/board/BoardUtils.java
@@ -1,6 +1,9 @@
 package ma.enset.chess.board;
 
 public class BoardUtils {
+    private BoardUtils() {
+        throw new RuntimeException("BoardUtils can't be instantiated");
+    }
     public static boolean isValidTileCoor(final int coordinates) {
         return coordinates >= 0 && coordinates <= 62;
     }
