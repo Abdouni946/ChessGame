12997b747718f153559a63fbdb80348572e367b7
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-20 21:38:47 +0100

    Added pawn attacking moves logice to calcLegalMoves method

--- src/ma/enset/chess/pieces/Pawn.java
+++ src/ma/enset/chess/pieces/Pawn.java
@@ -5,6 +5,7 @@ import ma.enset.chess.Alliance;
 import ma.enset.chess.board.Board;
 import ma.enset.chess.board.BoardUtils;
 import ma.enset.chess.board.Move;
+import ma.enset.chess.board.Move.AttackMove;
 import ma.enset.chess.board.Move.MajorMove;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class Pawn extends Piece {
-    private final static int[] LEGAL_MOVES_OFFSETS = { 8, 16 };
+    private final static int[] LEGAL_MOVES_OFFSETS = { 7, 8, 9, 16 };
     Pawn(final int position, final Alliance alliance) {
         super(position, alliance);
     }
@@ -21,7 +22,7 @@ public class Pawn extends Piece {
     public Collection<Move> calcLegalMoves(final Board board) {
         final List<Move> legalMoves = new ArrayList<>();
         for (final int candidateOffset : LEGAL_MOVES_OFFSETS) {
-            final int destinationCoordinate = this.position + (this.getAlliance().getDirection() * candidateOffset);
+            final int destinationCoordinate = this.position + (this.alliance.getDirection() * candidateOffset);
             if (!BoardUtils.isValidTileCoor(destinationCoordinate)) {
                 continue;
             }
@@ -30,11 +31,32 @@ public class Pawn extends Piece {
                 legalMoves.add(new MajorMove(board, this, destinationCoordinate));
             } else if (candidateOffset == 16 &&
                     this.isFirstMove() &&
-                    (BoardUtils.SECOND_ROW[this.position] && this.getAlliance().isBlack()) ||
-                    (BoardUtils.SEVENTH_ROW[this.position] && this.getAlliance().isWhite())) { // Pawn jump
-                final int behindDestinationCoor = this.position + (this.getAlliance().getDirection() * 8);
+                    (BoardUtils.SECOND_ROW[this.position] && this.alliance.isBlack()) ||
+                    (BoardUtils.SEVENTH_ROW[this.position] && this.alliance.isWhite())) { // Pawn jump
+                final int behindDestinationCoor = this.position + (this.alliance.getDirection() * 8);
                 if (!board.getTile(behindDestinationCoor).isOccupied() && !board.getTile(destinationCoordinate).isOccupied()) {
+                    // Needs to be changed !!
                     legalMoves.add(new MajorMove(board, this, destinationCoordinate));
+                }
+            } else if (candidateOffset == 7 &&
+                    !((BoardUtils.EIGHTH_COL[this.position] && this.alliance.isWhite()) ||
+                      (BoardUtils.FIRST_COL[this.position] && this.alliance.isBlack()))) { // Attacking pawn move 1
+                if (board.getTile(destinationCoordinate).isOccupied()) {
+                    final Piece occupingPiece = board.getTile(destinationCoordinate).getPiece();
+                    if (this.alliance != occupingPiece.getAlliance()) {
+                        // Needs to be changed !!
+                        legalMoves.add(new AttackMove(board, this, destinationCoordinate, occupingPiece));
+                    }
+                }
+            } else if (candidateOffset == 9 &&
+                    !((BoardUtils.FIRST_COL[this.position] && this.alliance.isWhite()) ||
+                      (BoardUtils.EIGHTH_COL[this.position] && this.alliance.isBlack()))) { // Attacking pawn move 2
+                if (board.getTile(destinationCoordinate).isOccupied()) {
+                    final Piece occupingPiece = board.getTile(destinationCoordinate).getPiece();
+                    if (this.alliance != occupingPiece.getAlliance()) {
+                        // Needs to be changed !!
+                        legalMoves.add(new AttackMove(board, this, destinationCoordinate, occupingPiece));
+                    }
                 }
             }
         }
--- src/ma/enset/chess/pieces/Piece.java
+++ src/ma/enset/chess/pieces/Piece.java
@@ -9,19 +9,23 @@ import java.util.Collection;
 public abstract class Piece {
     protected final int position;
     protected final Alliance alliance;
+    protected final boolean isFirstMove;
 
     Piece(final int position, final Alliance alliance) {
         this.position = position;
         this.alliance = alliance;
+        this.isFirstMove = false;
     }
 
     public int getPosition() {
-        return position;
+        return this.position;
     }
 
     public Alliance getAlliance() {
-        return alliance;
+        return this.alliance;
     }
+
+    public boolean isFirstMove() { return this.isFirstMove; }
 
     public abstract Collection<Move> calcLegalMoves(final Board board);
 }
