0fc60565e314c73dc2a984dfb9dee96c9c85deca
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-20 11:32:50 +0100

    Implemented Move class

--- src/ma/enset/chess/board/Move.java
+++ src/ma/enset/chess/board/Move.java
@@ -1,4 +1,15 @@
 package ma.enset.chess.board;
 
-public class Move {
+import ma.enset.chess.pieces.Piece;
+
+public abstract class Move {
+    private final Board board;
+    private final Piece movedPiece;
+    private final int destinationCoordinate;
+
+    public Move(Board board, Piece movedPiece, int destinationCoordinate) {
+        this.board = board;
+        this.movedPiece = movedPiece;
+        this.destinationCoordinate = destinationCoordinate;
+    }
 }
