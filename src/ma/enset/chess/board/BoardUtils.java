a672b4afc29b66f376fe0c9d2c0adbf0edff457c
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-20 10:23:09 +0100

    Added column exclusions

--- src/ma/enset/chess/board/BoardUtils.java
+++ src/ma/enset/chess/board/BoardUtils.java
@@ -1,10 +1,15 @@
 package ma.enset.chess.board;
 
 public class BoardUtils {
+    public static final boolean[] FIRST_COL = null;
+    public static final boolean[] SECOND_COL = null;
+    public static final boolean[] SEVENTH_COL = null;
+    public static final boolean[] EIGHTH_COL = null;
+
     private BoardUtils() {
         throw new RuntimeException("BoardUtils can't be instantiated");
     }
     public static boolean isValidTileCoor(final int coordinates) {
-        return coordinates >= 0 && coordinates <= 62;
+        return coordinates >= 0 && coordinates < 64;
     }
 }
--- src/ma/enset/chess/pieces/Knight.java
+++ src/ma/enset/chess/pieces/Knight.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Knight extends Piece {
-    private final static int[] LEGAL_MOVES_COORDINATES = { -17, -15, -10, -6, 6, 10, 15, 17 };
+    private final static int[] LEGAL_MOVES_OFFSETS = { -17, -15, -10, -6, 6, 10, 15, 17 };
 
     Knight(final int position, final Alliance alliance) {
         super(position, alliance);
@@ -21,9 +21,12 @@ public class Knight extends Piece {
     public List<Move> calcLegalMoves(Board board) {
         int destinationCoordinates;
         final List<Move> legalMoves = new ArrayList<>();
-        for (final int legalMoveCoordinates : LEGAL_MOVES_COORDINATES) {
-            destinationCoordinates = this.position + legalMoveCoordinates;
+        for (final int candidateOffset : LEGAL_MOVES_OFFSETS) {
+            destinationCoordinates = this.position + candidateOffset;
             if(BoardUtils.isValidTileCoor(destinationCoordinates)) {
+                if (isFirstColExclusion(this.position, candidateOffset) || isSecondColExclusion(this.position, candidateOffset) || isSeventhColExclusion(this.position, candidateOffset) || isEighthColExclusion(this.position, candidateOffset) ||) {
+                    continue;
+                }
                 final Tile destinationTile = board.getTile(destinationCoordinates);
                 if(!destinationTile.isOccupied()) {
                     legalMoves.add(new Move());
@@ -36,7 +39,19 @@ public class Knight extends Piece {
                 }
             }
         }
-
         return ImmutableList.copyOf(legalMoves);
+    }
+
+    private static boolean isFirstColExclusion(final int currentPos, final int offset) {
+        return BoardUtils.FIRST_COL[currentPos] && ((offset == -17) || (offset == -10) || (offset == 6) || (offset == 15));
+    }
+    private static boolean isSecondColExclusion(final int currentPos, final int offset) {
+        return BoardUtils.SECOND_COL[currentPos] && ((offset == -10) || (offset == 6));
+    }
+    private static boolean isSeventhColExclusion(final int currentPos, final int offset) {
+        return BoardUtils.SEVENTH_COL[currentPos] && ((offset == -6) || (offset == 10));
+    }
+    private static boolean isEighthColExclusion(final int currentPos, final int offset) {
+        return BoardUtils.EIGHTH_COL[currentPos] && ((offset == -15) || (offset == -6) || (offset == 10) || (offset == 17));
     }
 }
