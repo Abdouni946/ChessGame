372ab3582285d760b369798e9983955311ff5514
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-20 20:36:43 +0100

    Added method to get the direction of the alliance in the Alliance ENUM

--- src/ma/enset/chess/Alliance.java
+++ src/ma/enset/chess/Alliance.java
@@ -1,7 +1,18 @@
 package ma.enset.chess;
 
 public enum Alliance {
-    WHITE,
-    BLACK
+    WHITE {
+        @Override
+        int getDirection() {
+            return -1;
+        }
+    },
+    BLACK {
+        @Override
+        int getDirection() {
+            return 1;
+        }
+    };
 
+    abstract int getDirection();
 }
