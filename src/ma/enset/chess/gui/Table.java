61448376a414fb41874d3721f64139c03f5dbd96
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-27 21:43:41 +0100

    Implemented assignTileColor method

--- src/ma/enset/chess/gui/Table.java
+++ src/ma/enset/chess/gui/Table.java
@@ -11,16 +11,23 @@ import java.util.List;
 
 public class Table {
     private final JFrame gameFrame;
+    private final BoardPanel boardPanel;
 
     private static final Dimension OUTER_FRAME_DIMENSION = new Dimension(800, 800);
     private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(600, 600);
     private static final Dimension TILE_PANEL_DIMENSION = new Dimension(75, 75);
+    
+    private Color lightTileColor = Color.decode("#FFFACD");
+    private Color darkTileColor = Color.decode("#593E1A");
 
     public Table() {
         this.gameFrame = new JFrame("Chess");
+        this.gameFrame.setLayout(new BorderLayout());
         final JMenuBar tableMenuBar = createMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
         this.gameFrame.setVisible(true);
     }
 
@@ -50,7 +57,7 @@ public class Table {
             super(new GridLayout(8, 8));
             this.tilesPanels = new ArrayList<>();
             for (int i = 0; i < BoardUtils.NUM_TILES; ++i) {
-                final TilePanel tilePanel = new TilePanel();
+                final TilePanel tilePanel = new TilePanel(this, i);
                 this.tilesPanels.add(tilePanel);
                 add(tilePanel);
             }
@@ -68,6 +75,20 @@ public class Table {
             setPreferredSize(TILE_PANEL_DIMENSION);
             assignTileColor();
             validate();
+        }
+
+        private void assignTileColor() {
+            if (BoardUtils.FIRST_ROW[this.tileId] ||
+                    BoardUtils.THIRD_ROW[this.tileId] ||
+                    BoardUtils.FIFTH_ROW[this.tileId] ||
+                    BoardUtils.SEVENTH_ROW[this.tileId]) {
+                setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
+            } else if (BoardUtils.SECOND_ROW[this.tileId] ||
+                    BoardUtils.FOURTH_ROW[this.tileId] ||
+                    BoardUtils.SIXTH_ROW[this.tileId] ||
+                    BoardUtils.EIGHTH_ROW[this.tileId]) {
+                setBackground(this.tileId % 2 != 0 ? lightTileColor : darkTileColor);
+            }
         }
     }
 }
