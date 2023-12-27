963e569abc43e7717647ce04f3c7ebf008c53ccb
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-12-27 21:02:33 +0100

    Added initial menu bar

--- src/ma/enset/chess/gui/Table.java
+++ src/ma/enset/chess/gui/Table.java
@@ -2,6 +2,8 @@ package ma.enset.chess.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Table {
     private final JFrame gameFrame;
@@ -10,7 +12,27 @@ public class Table {
 
     public Table() {
         this.gameFrame = new JFrame("Chess");
+        final JMenuBar tableMenuBar = new JMenuBar();
+        populateMenuBar(tableMenuBar);
+        this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
         this.gameFrame.setVisible(true);
+    }
+
+    private void populateMenuBar(final JMenuBar tableMenuBar) {
+        tableMenuBar.add(createFileMenu());
+    }
+
+    private JMenu createFileMenu() {
+        final JMenu fileMenu = new JMenu("File");
+        final JMenuItem openPGN = new JMenuItem("Load PGN file");
+        openPGN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Open PGN file");
+            }
+        });
+        fileMenu.add(openPGN);
+        return fileMenu;
     }
 }
